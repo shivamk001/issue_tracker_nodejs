312723107944c554ccab6b4eb3d5f2a6e3edf2dd
shivamk001 <shivam.kesarwani001@gmail.com>
2025-04-08 23:05:31 +0530

    set mongo_url

--- index.js
+++ index.js
@@ -26,7 +26,6 @@ app.set('layout extractStyles', true)
 
 //mongodb
 require('./config/mongoose')
-console.log('MONGODB:', process.env.MONGO_URL, `${process.env.MONGO_URL}?retryWrites=true&w=majority`);
 
 //passport
 //session
@@ -36,7 +35,7 @@ app.use(session({
     saveUninitialized: false,
     // store: MongoStore.create({
         // mongoUrl:'mongodb+srv://shivamk001:4gtBAfKze5K1D2wJ@cluster0.r6rocwl.mongodb.net/issueTracker?retryWrites=true&w=majority',
-        mongoUrl:`mongodb://issuetracker-mongo-srv:27017/issuetracker?retryWrites=true&w=majority`
+        mongoUrl:`${process.env.MONGO_URL}?retryWrites=true&w=majority`
     // })
 }))
 app.use(passport.authenticate('session'));
